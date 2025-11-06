ea19ad450cdba9cab28c41ec3e32dab44c11051f
Reggie <reglitster00@gmail.com>
2025-11-06 10:51:37 -0500

    Make Abstract Shape Class

---
+++ src/main/java/com/pluralsight/shapes/Shape.java
@@ -0,0 +1,18 @@
+package com.pluralsight.shapes;
+
+import com.pluralsight.Turtle;
+
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+public abstract class Shape {
+
+protected Turtle turtle;
+protected Point2D.Double location;
+protected Color color;
+protected Border border;
+//protected
+
+
+}
